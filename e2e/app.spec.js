b23f4f30b00b185919a3a9766d9bafe860408856
Vincent Behar <vincent.behar@dailymotion.com>
2019-02-14 11:32:27 +0100

    disable chromium sandbox

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -2,7 +2,12 @@ const puppeteer = require('puppeteer');
 const ui = process.env.UI || 'http://127.0.0.1:3000';
 
 it('h1 loads correctly', async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]
+    });
     const page = await browser.newPage();
     await page.goto(ui);
     await page.waitFor('.App');
